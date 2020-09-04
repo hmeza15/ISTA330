904e212a210ccf441b330e5ec9ab0306f3d2a7cb
hmeza15 <hmeza@email.arizona.edu>
2020-09-03 22:07:26 -0700

    update9

--- practicum/week-1/question9.js
+++ practicum/week-1/question9.js
@@ -7,7 +7,13 @@ input: 'llheo', shuffleIndices = [2, 3, 0, 1, 4]
 output: 'hello'
 
 */
-
+//whatever the value is in shuffIndices is the index in the string input
+//'hello' is ['h', 'e', 'l', 'l', 'o']
 var suffleString = function(input, shuffleIndices) {
-
+    let string = ""
+    for (let i = 0; i < shuffleIndices.length; i++) {
+        string = string + input[shuffleIndices[i]]; //string + input[2] (shuffleindices value at index 0)
+                                                    //string + 'h' (input value at index 2)
+    }
+    return string;
 };
