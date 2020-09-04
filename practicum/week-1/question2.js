14af054dfd17b98a52a5319e5f6d0b068ad055c8
hmeza15 <hmeza@email.arizona.edu>
2020-09-04 15:37:34 -0700

    update2

--- practicum/week-1/question2.js
+++ practicum/week-1/question2.js
@@ -3,15 +3,13 @@
 The array cookies represent the number of cookies that each kid has. 
 Given the array cookies and an integer extraCookies, determine whether there is a
 way to distribute the extraCookies among the kids in a way that kids get equal number of cookies.
-Example: input: cookies = [3, 5, 8] , extraCookies = 8
+Example: input: cookies = [3, 5, 8] , extraCookies = 11
          output: true
          Because we can give 5 of the extraCookies to the first kid and the remaining 3 to the second kid
          and therfore each of the three kids will get 8 cookies.
  */
 
  var canGetEqualCookies = function(cookies, extraCookies) {
-     //added all numbers in array then added with extra cookies = total amt cookies
-     //mod % to get remainder and if remainder=0 no extra cookies
      //return false if remainder doesnt equal 0 then we can't have equal cookies for everyone
         let max = 0;
         let index = 0;
@@ -32,5 +30,34 @@ Example: input: cookies = [3, 5, 8] , extraCookies = 8
                       
                 }
         }
+        if (extraCookies > 0) {
+                for (index = 0; index < cookies.length; index++) {
+                        cookies[index] += 1;
+                        extraCookies -= 1;
+                        if (extraCookies == 0){
+                                break;
+                        }
+                }
+
+        }
+        if (extraCookies == 0) {
+                for (index = 0; (index + 1) < cookies.length; index++){
+                        if (cookies[index] != cookies[index + 1]) {
+                                return false;
+                        }
+                }
+        }
         return true;
  };
+let result = canGetEqualCookies([3,5,2], 6);
+console.log(result)
+ /*let hasequalnumbers = function(input){
+        //for(let index = 0; (index + 1) < input.length; index++) {
+                //if (input[index] != input[index + 1]) {
+                      //  return false;
+                //}
+        //}
+        return true;
+ };
+ let result = hasequalnumbers([1 ,1, 1]);
+ console.log(result) */
