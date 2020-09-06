0608d238c74629dcb4a624b801a05f6ac1b42342
hmeza15 <hmeza@email.arizona.edu>
2020-09-05 18:28:59 -0700

    update9

--- practicum/week-1/question9.js
+++ practicum/week-1/question9.js
@@ -8,12 +8,30 @@ output: 'hello'
 
 */
 //whatever the value is in shuffIndices is the index in the string input
-//'hello' is ['h', 'e', 'l', 'l', 'o']
+//'hello' is ['h', 'e', 'l', 'l', 'o'] - its an array
 var suffleString = function(input, shuffleIndices) {
-    let string = ""
-    for (let i = 0; i < shuffleIndices.length; i++) {
-        string = string + input[shuffleIndices[i]]; //string + input[2] (shuffleindices value at index 0)
-                                                    //string + 'h' (input value at index 2)
+    //sort and remove smallest value from shuffleIndices into temparray
+    //look through array find smallest value then save into empty array
+    let string = "";
+    while (string.length < input.length) { //shrinking array/deleting every min to find new min
+        let pos = 0; //saving position inside shuffleIndices that had the lowest value
+        let min = shuffleIndices[0]; //holding minimum in shuffleIndices
+            for (let i = 1; i < shuffleIndices.length; i++) {
+                if (min == -1) { //disregard each letter at a time
+                    min = shuffleIndices[i];
+                    pos = i;
+                }
+                if (shuffleIndices[i] < min && shuffleIndices[i] != -1) { 
+                    min = shuffleIndices[i]; //temp = i saving position of the minimum value
+                    pos = i;
+                }
+            }
+        string = string + input[pos];
+        shuffleIndices[pos] = -1;
     }
     return string;
 };
+//let result = suffleString('emelwco', [1, 5, 6, 2, 0, 3, 4]);
+//console.log(result);
+//let result2 = suffleString('llheo', [2, 3, 0, 1, 4]);
+//console.log(result2);
