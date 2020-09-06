6ca830f5e2a3cb817312ca799dd9c224f126637d
hmeza15 <hmeza@email.arizona.edu>
2020-09-05 18:50:32 -0700

    update10

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -7,5 +7,27 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
+    //while loop to find max then another that finds the min number then subtract them
+    let max = -1;
+    let min = -1; //for min nothing is lower than 0
+    while (n != 0) {
+        let temp = n % 10; //strips last character off and holds last value
 
+        if (min == -1) {
+            min = temp;
+        } else if (temp < min) {
+            min = temp;
+        }
+
+        if (max == -1) {
+            max = temp;
+        } else if (temp > max) {
+            max = temp;
+        }
+
+        n = n / 10; //strips off last character/ keeps shifting last character to right to keep checking
+    }
+    return (max - min);
 };
+let result = maxMinusMin(472);
+console.log(result);
